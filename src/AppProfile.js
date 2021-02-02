9b32a2d3c25f22649d30b30f363d2c87cdfcc9ea
phan thien <thienphan9612@gmail.com>
2021-02-02 08:46:03 +0700

    abc

--- src/AppProfile.js
+++ src/AppProfile.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
 import { CSSTransition } from 'react-transition-group';
+import { Link, useHistory} from "react-router-dom";
 
 export const AppProfile = () => {
-
+    let history = useHistory();
     const [expanded, setExpanded] = useState(false);
 
     const onClick = (event) => {
         setExpanded(prevState => !prevState);
         event.preventDefault();
+    }
+
+    const logOutAction = () => {
+        sessionStorage.usn = undefined;
+        history.push('/login');
+        window.location.reload();
     }
 
     return (
@@ -24,7 +31,11 @@ export const AppProfile = () => {
                 <ul className={classNames({ 'layout-profile-expanded': expanded })}>
                     <li><button type="button" className="p-link"><i className="pi pi-fw pi-user" /><span>Account</span></button></li>
                     <li><button type="button" className="p-link"><i className="pi pi-fw pi-inbox" /><span>Notifications</span><span className="menuitem-badge">2</span></button></li>
-                    <li><button type="button" className="p-link"><i className="pi pi-fw pi-power-off" /><span>Logout</span></button></li>
+                    <li>
+                        <Link to="/login">
+                            <button type="button" className="p-link" onClick={logOutAction}><i className="pi pi-fw pi-power-off" /><span>Logout</span></button>
+                        </Link>
+                    </li>
                 </ul>
             </CSSTransition>
         </div>
